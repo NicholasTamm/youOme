cd6b1440d3f81e3289914030850db173bf461d89
Nicholas Tam <nta59@sfu.ca>
2025-10-13 20:07:59 -0700

    [FEAT] Add RankingsFragment and update rankings icon; replace previous debts design

--- app/src/main/java/com/example/youome/RankingsFragment.kt
+++ app/src/main/java/com/example/youome/RankingsFragment.kt
@@ -6,12 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class DebtFragment : Fragment() {
+class RankingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.debts, container, false)
+        return inflater.inflate(R.layout.rankings, container, false)
     }
 }
